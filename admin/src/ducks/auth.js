3231d430ef63a472af233a8d1c03ee318756ec46
Banifaciy <aleksey.nov@gmail.com>
2018-03-12 22:39:54 +0300

    Auth realtime via eventChannel

--- admin/src/ducks/auth.js
+++ admin/src/ducks/auth.js
@@ -1,4 +1,5 @@
-import {all, takeEvery, take, put, apply, call} from 'redux-saga/effects'
+import {all, takeEvery, take, put, apply, call, spawn} from 'redux-saga/effects'
+import {eventChannel} from 'redux-saga'
 import {appName} from '../config'
 import {createSelector} from 'reselect'
 import {Record} from 'immutable'
@@ -84,12 +85,6 @@ export function signUp(email, password) {
     }
 }
 
-firebase.auth().onAuthStateChanged(user => {
-    if (user) window.store.dispatch({
-        type: SIGN_IN_SUCCESS,
-        payload: { user }
-    })
-})
 
 /**
  * Sagas
@@ -142,8 +137,33 @@ export function * watchStatusChangeSaga() {
     }
 }
 
+const createAuthChannel = () => eventChannel(emit => {
+	const callback = (user) => emit({ user })
+	firebase.auth().onAuthStateChanged(callback)
+
+	// TODO: fix it
+	return () => firebase.auth().onAuthStateChanged(callback)
+})
+
+export function * authRealtime() {
+	const channel = yield call(createAuthChannel)
+
+	while (true) {
+		const { user } = yield take(channel)
+
+		if (user) {
+			yield put({
+				type: SIGN_IN_SUCCESS,
+				payload: { user }
+			})
+		}
+	}
+}
+
 
 export const saga = function * () {
+	yield spawn(authRealtime)
+
     yield all([
         takeEvery(SIGN_IN_REQUEST, signInSaga),
         signUpSaga(),
